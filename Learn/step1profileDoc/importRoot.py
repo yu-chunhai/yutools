62ecc082d6c0343a6d772a57c8ee0736ba1c9535
yu-chunhai <18905255025@163.com>
2018-03-12 08:12:32 +0800

    20180312Appium-Python-Client-0.26

--- Learn/step1profileDoc/importRoot.py
+++ Learn/step1profileDoc/importRoot.py
@@ -3,3 +3,4 @@
 #@Time:2018/3/11-14:54
 #@文件名称:yu-chunhai-importRoot
 selenium390 = r'C:\yutools\testTools\py3ImportRoot\selenium-3.9.0'
+AppiumPythonClient026 = r'C:\yutools\testTools\py3ImportRoot\Appium-Python-Client-0.26'
