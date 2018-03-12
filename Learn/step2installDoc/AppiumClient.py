63018a929ecc37dd0c10c0545e83680d5eb2a260
yu-chunhai <18905255025@163.com>
2018-03-12 08:15:24 +0800

    20180312Appium-Python-Client-0.26

---
+++ Learn/step2installDoc/AppiumClient.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# @作者:Administrator
+#@Time:2018/3/12-8:13
+#@文件名称:yutools-AppiumClient
+import os
+import Learn.step1profileDoc.importRoot
+AppiumPythonClient026Root = Learn.step1profileDoc.importRoot.AppiumPythonClient026
+os.chdir(AppiumPythonClient026Root)
+os.system('python setup.py install')
